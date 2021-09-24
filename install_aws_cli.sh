8a070564556043ddf45d003fb4b00d436e0aa07e
jesus-rosas-msicu <71845938+jesus-rosas-msicu@users.noreply.github.com>
2021-09-24 17:11:42 -0500

    AWS Command Line installation
    Small script to install the aws command line version 2.0

---
+++ install_aws_cli.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Installing aws command line"
+sudo apt install zip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+aws --version
